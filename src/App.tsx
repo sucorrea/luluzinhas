f49f9e784c6e1c424aea396d5dc8438fe68ac87f
sueli.oliveira <sueli.oliveira@newconsoftware.com.br>
2023-03-09 16:12:22 -0300

    fonte

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 import React from 'react'
 
 import ListaAniversariantes from './ListaAniversariantes'
+import { createTheme, makeStyles, ThemeProvider } from '@mui/material'
+
+import './index.css'
+const theme = createTheme({
+  typography: { fontFamily: ['Rubik', 'sans-serif'].join(',') },
+})
 
 const App = () => {
-  return <ListaAniversariantes />
+  return (
+    <ThemeProvider theme={theme}>
+      <ListaAniversariantes />
+    </ThemeProvider>
+  )
 }
 
 export default App
